f8f68362f94c7ba8cec3035b57e4efd2ce9e42a5
ldd233 <332922303@qq.com>
2022-05-10 17:43:08 +0800

    modify

--- MultiAccountTrade/constant.py
+++ MultiAccountTrade/constant.py
@@ -1,6 +1,6 @@
-import enum, datetime
-from typing import Tuple
-from vnpy.trader.constant import Exchange
+import datetime
+
+from enum import Enum
 
 DAY_START = datetime.time(8, 45)
 DAY_END = datetime.time(15, 0)
@@ -48,67 +48,8 @@ TRADE_TIME_SETTING = {
 }
 
 
-class OrderMode(enum.Enum):
+class OrderMode(Enum):
     BUY = "OPEN_BUY"
     SELL = "CLOSE_SELL"
     SHORT = "OPEN_SELL"
     COVER = "CLOSE_BUY"
-
-
-class OrderRequest:
-    def __init__(self, ContractID: str, Op1: str, Op2: str, Num: float) -> None:
-        self.vt_symbol = self.convert_to_vt_symbol(ContractID)
-        self.order_mode = self.convert_to_vt_order_mode(Op1, Op2)
-        self.volume = Num
-
-    @classmethod
-    def convert_to_vt_symbol(self, symbol: str) -> str:
-        spl = symbol.split(".")
-        pre = spl[0].lower()
-        suf = spl[1]
-
-        if suf == "CZC":
-            pre = pre.upper()
-            suf = Exchange.CZCE.value
-
-        elif suf == "SHF":
-            suf = Exchange.SHFE.value
-
-        return f"{pre}.{suf}"
-
-    @classmethod
-    def convert_to_symbol(self, vt_symbol: str) -> str:
-        spl = vt_symbol.split(".")
-        pre = spl[0].upper()
-        suf = spl[1]
-
-        if suf == Exchange.CZCE.value:
-            suf = "CZC"
-        elif suf == Exchange.SHFE.value:
-            suf = "SHF"
-
-        return f"{pre}.{suf}"
-
-    @classmethod
-    def convert_to_vt_order_mode(self, Op1: str, Op2: str) -> OrderMode:
-        if Op1 == "Open":
-            if Op2 == "Buy":
-                return OrderMode.BUY
-            else:
-                return OrderMode.SHORT
-        else:
-            if Op2 == "Buy":
-                return OrderMode.COVER
-            else:
-                return OrderMode.SELL
-
-    @classmethod
-    def convert_to_order_mode(self, order_mode: OrderMode) -> Tuple[str, str]:
-        if order_mode == OrderMode.BUY:
-            return "Open", "Buy"
-        elif order_mode == OrderMode.SHORT:
-            return "Open", "Sell"
-        elif order_mode == OrderMode.COVER:
-            return "Close", "Buy"
-        elif order_mode == OrderMode.SELL:
-            return "Close", "Sell"
--- MultiAccountTrade/twap.py
+++ MultiAccountTrade/twap.py
@@ -4,7 +4,8 @@ from typing import Any, List, Dict
 from pandas import DataFrame
 
 from engine import BackupEngine, MAEngine
-from constant import OrderMode, OrderRequest
+from constant import OrderMode
+from object import OrderRequest
 
 from vnpy.trader.object import TradeData
 
@@ -12,10 +13,7 @@ class TWAP():
     def __init__(self, engine: MAEngine, gateway_name: str, request: OrderRequest, setting: Dict[str, int] = None) -> None:
         self.engine: MAEngine = engine
         self.gateway_name: str = gateway_name
-
-        self.vt_symbol: str = request.vt_symbol
-        self.order_mode: OrderMode = request.order_mode
-        self.volume: float = request.volume
+        self.request: OrderRequest = request
         
         self.time: int = setting.get("TIME")
         self.interval: int = setting.get("INTERVAL")
@@ -25,7 +23,7 @@ class TWAP():
         self.twap_volume: float = self.get_twap_volume()
 
     async def run(self) -> None:
-        while self.traded_volume < self.volume:
+        while self.traded_volume < self.request.volume:
             self.send_order()
             await asyncio.sleep(self.interval)
             self.cancel_active_orders()
@@ -40,15 +38,15 @@ class TWAP():
                 )
             
     def send_order(self) -> List[str]:
-        volume = min(self.twap_volume, self.volume - self.traded_volume)
+        volume = min(self.twap_volume, self.request.volume - self.traded_volume)
 
-        if self.order_mode == OrderMode.BUY:
+        if self.request.order_mode == OrderMode.BUY:
             self.vt_orderids = self.engine.buy(self.vt_symbol, volume, self.gateway_name)
-        elif self.order_mode == OrderMode.SELL:
+        elif self.request.order_mode == OrderMode.SELL:
             self.vt_orderids = self.engine.sell(self.vt_symbol, volume, self.gateway_name)
-        elif self.order_mode == OrderMode.SHORT:
+        elif self.request.order_mode == OrderMode.SHORT:
             self.vt_orderids = self.engine.short(self.vt_symbol, volume, self.gateway_name)
-        elif self.order_mode == OrderMode.COVER:
+        elif self.request.order_mode == OrderMode.COVER:
             self.vt_orderids = self.engine.cover(self.vt_symbol, volume, self.gateway_name)
 
     def cancel_active_orders(self) -> None:
@@ -62,7 +60,7 @@ class TWAP():
                 self.traded_volume += trade.volume
 
     def get_twap_volume(self) -> float:
-        return max(float(math.floor(self.volume / (self.time / self.interval))), 1.0)
+        return max(float(math.floor(self.request.volume / (self.time / self.interval))), 1.0)
 
 
 def backup(engine: MAEngine, gateway_name: str, vt_symbol: str, order_mode: OrderMode, left_volume: float):
