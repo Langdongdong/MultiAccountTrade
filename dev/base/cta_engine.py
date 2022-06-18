ff4d51d01a786b2a6dfff843b16fe965f2c45500
ldd233 <332922303@qq.com>
2022-06-18 20:30:14 +0800

    delete files

--- dev/base/cta_engine.py
+++
@@ -1,8 +0,0 @@
-
-from engine import BaseEngine, MainEngine
-from vnpy.event import EventEngine
-
-
-class CtaEngine(BaseEngine):
-    def __init__(self, main_engine: MainEngine, event_engine: EventEngine, engine_name: str) -> None:
-        super().__init__(main_engine, event_engine, engine_name)
